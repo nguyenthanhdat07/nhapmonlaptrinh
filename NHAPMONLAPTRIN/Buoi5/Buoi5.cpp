0cde1603243cbec8f497bf462ef3c7693c224cd7
Buoi4 <Administrator@PMT2021>
2025-12-04 14:27:39 +0700

    Buoi5

--- NHAPMONLAPTRIN/Buoi5/Buoi5.cpp
+++ NHAPMONLAPTRIN/Buoi5/Buoi5.cpp
@@ -1,1 +1,113 @@
-      
+#include <iostream>
+using namespace std;
+void cau1()
+{   
+	int a, b, c;
+	cout << "Nhap a, b, c: ";
+	cin >> a >> b >> c;
+	if (a > b && a > c)
+	{
+		cout << a;
+	}
+	if (b > a && b > c)
+	{
+		cout << b;
+	}
+	if (c > a && c > b)
+	{
+		cout << c;
+	}
+}
+
+void cau2()
+{
+	float a, b;
+	cout << "Nhap a, b: ";
+	cin >> a >> b;
+	if (a * b > 0)
+	{
+		cout << "cung dau";
+	}
+	else
+	{
+		cout << "khac dau";
+	}
+}
+
+void cau3()
+{
+	int a, b;
+	cout << "Nhap a, b: ";
+	cin >> a >> b;
+	if (a == 0)
+	{
+		if (b == 0)
+		{
+			cout << "Phuong trinh vo so nghiem" << endl;
+		}
+		else
+		{
+			cout << "Phuong trinh vo nghiem" << endl;
+		}
+	}
+	else
+	{
+		cout << "Nghiem la: " << (-b * 1.0 / a) << endl;
+	}
+}
+void cau4()
+{
+	int thang;
+	cout << "Nhap thang: ";
+	cin >> thang;
+	if (thang <= 3)
+	{
+		cout << "Quy 1" << endl;
+	}
+	else if (thang <= 6)
+	{
+		cout << "Quy 2" << endl;
+	}
+	else if (thang <= 9)
+	{
+		cout << "Quy 3" << endl;
+	}
+	else if (thang <= 12)
+	{
+		cout << "Quy 4" << endl;
+	}
+	else if (thang > 12)
+	{
+		cout << "loi" << endl;
+	}
+}
+
+void cau5()
+{
+	int a, b, c;
+	cout << "Nhap 3 canh a, b, c: ";
+	cin >> a >> b >> c;
+	if ((a + b > c) && (b + c > a) && (a + c > b))
+	{
+		if (a == b && b == c)
+		{
+			cout << "Tam giac deu" << endl;
+		}
+		else if (a == b || b == c || c == a)
+		{
+			cout << "Tam giac can" << endl;
+		}
+		else if ((b * b == a * a + c * c) || (a * a == b * b + c * c) || (c * c == a * a + b * b))
+		{
+			cout << "Tam giac vuong" << endl;
+		}
+	}
+	else {
+		cout << "Khong la tam giac" << endl;
+	}
+}
+
+
+int main() {
+	cau4();
+}
