99f07ceaef80178d4f44d973a53ad32999c4190a
Buoi8 <Administrator@PMT2021>
2025-12-18 16:00:11 +0700

    Buoi12

---
+++ NHAPMONLAPTRIN/Buoi8/Buoi8.cpp
@@ -0,0 +1,15 @@
+#include "Array.h"
+#define MAX 100
+
+int main()
+{
+	int a[MAX], n = 0;
+	input(a, n);
+	//output(a, n);
+	//giatrichan(a, n);
+	//vitriam(a, n);
+	//giatrilonnhat(a, n);
+	//vitrinhonhat(a,n);
+	nguyentonhohon100(a, n);
+
+}
